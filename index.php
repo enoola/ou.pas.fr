0400b211ad1117682cf0fd1a57100a6787785dbc
enoola <enola@next.local>
2024-11-29 00:06:04 +0100

    correction to main index

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
 
 // List of HTML files
-$html_files = [ 'index1.html',
-    'index2.html',
-    'index3.html'
+$html_files = [ 'index1_anim_nb_cartoon.html',
+    'index2_font_matrix.html',
+    'index3_font_matrix_colorized.html',
+    'index3_font_matrix_colorized2.html',
+    'index4_galaxyRevealText.html',
+    'typescript_machine_bw/idx_typescript_machine_bw.html',
+    'balls_falling_and_bouncing/balls_falling_and_bouncing.html'
 ];
 
 // Randomly select a file from the list
